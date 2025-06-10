a88096ba6bcbf341ea5f1130b3a7e3c23411ec18
returnvalue <chris.fiandaca@outlook.com>
2025-06-10 19:44:14 -0400

    Refactor hello script into greet_user function

--- hello.py
+++ hello.py
@@ -1,2 +1,8 @@
-name = input("What is your name? ")
-print(f"Hello, {name}!")
+def greet_user():
+    """Prompt for a user's name and greet them."""
+    name = input("What is your name? ")
+    print(f"Hello, {name}!")
+
+
+if __name__ == "__main__":
+    greet_user()
